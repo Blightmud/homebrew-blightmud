b60a1df0205949cd296847bcaf902324e3c2e2a9
Linus Probert <linus.probert@gmail.com>
2021-02-01 09:09:40 +0100

    Version 3.0.2

--- Formula/blightmud.rb
+++ Formula/blightmud.rb
@@ -1,12 +1,12 @@
 class Blightmud < Formula
   desc "Terminal mud client written in Rust"
   homepage "https://github.com/Blightmud/Blightmud"
-  version "3.0.1"
+  version "3.0.2"
   license "GPL-3.0-only"
   bottle :unneeded
 
   url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos.zip"
-  sha256 "dd8d8d966bccd4f1e81510436a7929074e246da8a883a7edf610c5d90bb59155"
+  sha256 "eb0c3faf1c34f32aa5d98e0eec8ae027d1804b6fd1502c39b3e1b287ab7e1824"
 
   def install
     bin.install "blightmud"
