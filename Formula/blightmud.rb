a3d1b2454057e4dd57becfb3021cc1e80e7125a9
Linus Probert <linus.probert@gmail.com>
2021-09-21 06:52:08 +0200

    Fixes openssl dependency

--- Formula/blightmud.rb
+++ Formula/blightmud.rb
@@ -4,6 +4,7 @@ class Blightmud < Formula
   version "3.3.0"
   license "GPL-3.0-only"
   bottle :unneeded
+  depends_on "openssl"
 
   url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos.zip"
   sha256 "15413890e2cc5b6577ff4b41b34d1d11e9e7fc3a67e7f6adee7835704574bff6"
