ac49256fb055ee3f492dbc10343b3eaf1a3a746d
Jimmy Lim <mirageglobe@gmail.com>
2023-03-29 10:01:03 +0100

    Add macos specific options to allow different builds
    - fix for https://github.com/Blightmud/Blightmud/issues/768

--- Formula/blightmud.rb
+++ Formula/blightmud.rb
@@ -5,8 +5,17 @@ class Blightmud < Formula
   license "GPL-3.0-only"
   depends_on "openssl"
 
-  url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos.zip"
-  sha256 "c6c325c5b6ad67fcb2b7eb7cadf63fbdda00bfae3d0ce26c50f83743ef8e5805"
+  if OS.mac?
+    on_big_sur do
+      url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos-11.zip"
+      sha256 "8f6e089a59745afe6c07d82c9db7417e728bdab550e25db3d3c82aac8a872939"
+    end
+
+    on_monterey :or_newer do
+      url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos.zip"
+      sha256 "c6c325c5b6ad67fcb2b7eb7cadf63fbdda00bfae3d0ce26c50f83743ef8e5805"
+    end
+  end
 
   def install
     bin.install "blightmud"
