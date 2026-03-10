ef3ef6fbb3929af3fb123b3168747848d45a3f4a
Linus Probert <linus.probert@gmail.com>
2026-03-10 21:48:47 +0100

    Version 5.5.0

--- Formula/blightmud.rb
+++ Formula/blightmud.rb
@@ -1,19 +1,19 @@
 class Blightmud < Formula
   desc "Terminal mud client written in Rust"
   homepage "https://github.com/Blightmud/Blightmud"
-  version "5.4.1"
+  version "5.5.0"
   license "GPL-3.0-only"
   depends_on "openssl"
 
   if OS.mac?
     on_big_sur do
       url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos-11.zip"
-      sha256 "03a533703720a8748a04e25484396b1ef070fc1ca55896939a18e4db0d4afa32"
+      sha256 "313f310e54d79047c231fcfd0363da428f4e5dfc4a1e76a7139f7346b6f2b74a"
     end
 
     on_monterey :or_newer do
       url "https://github.com/Blightmud/Blightmud/releases/download/v#{version}/blightmud-v#{version}-macos.zip"
-      sha256 "03a533703720a8748a04e25484396b1ef070fc1ca55896939a18e4db0d4afa32"
+      sha256 "313f310e54d79047c231fcfd0363da428f4e5dfc4a1e76a7139f7346b6f2b74a"
     end
   end
 
